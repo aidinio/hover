5da5f2e9eeb978e58a04c56745f2badf518e69d7
aidinio <aidin.webs@gmail.com>
2024-07-18 21:04:54 +0330

    Polished the UI

--- widget/main/silent.lua
+++ widget/main/silent.lua
@@ -1,20 +1,10 @@
 gears = require("gears")
 switch = require("hover.widget.wrapper.switch")
 
-local custom_event = "my_event"
-
-function event_handler()
-	naughty.notify({title = "Event triggered!"})
-end
-
-awesome.connect_signal(custom_event, event_handler)
-
 function silent()
    local silent_widget = switch("bell-slash").widget
    silent_widget.buttons  = gears.table.join(
 	  awful.button({}, 1, function ()
-			naughty.notify {title = "Hello"}
-			awesome.emit_signal(custom_event)
             silent_widget:toggle()
 	  end)
    )
